0ef5a33e848127a6d06cd954d3b553bb966c0b3c
Alejandro <alejandrorodriguezalcala1@gmail.com>
2025-04-25 10:19:05 +0100

    Rename File & Add Enum Camera Type

---
+++ packages/render/src/components/viewport.tsx
@@ -0,0 +1,114 @@
+import React, { useRef, useEffect, useState } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  OrthographicCamera,
+} from "@react-three/drei";
+import { Importer } from "./importer.tsx";
+import { OrbitLogger } from "./orbitLogger.tsx";
+import { useRenderSelector, useRenderDispatch } from "../store/hooks.ts";
+import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
+import {
+  initWebGPURenderer,
+  resetCameras,
+  repositionCameras,
+  CameraSwitcher,
+  ECameraType,
+} from "../utils/scene.tsx";
+import { recordRotationChange } from "../utils/orbit.tsx";
+import { zoomLevel } from "../constants/zoomLevel.ts";
+
+function Viewport() {
+  const controlsRef = useRef<OrbitControlsImpl | null>(null);
+  const perspectiveCamRef = useRef();
+  const orthoCamRef = useRef();
+  const modelData = useRenderSelector((state) => state.controls.modelData);
+  const file = useRenderSelector((state) => state.file.data);
+  const dispatch = useRenderDispatch();
+
+  const [cameraType, setCameraType] = useState<ECameraType>(ECameraType.PERSP);
+
+  const toggleCamera = () => {
+    setCameraType((prev) =>
+      prev === ECameraType.PERSP ? ECameraType.ORTHO : ECameraType.PERSP
+    );
+  };
+
+  //TODO: add the missing parameter enum
+  useEffect(() => {
+    repositionCameras(
+      file,
+      modelData,
+      perspectiveCamRef.current,
+      orthoCamRef.current,
+      cameraType
+    );
+  }, [modelData, cameraType]);
+
+  useEffect(() => {
+    resetCameras(
+      file,
+      perspectiveCamRef.current,
+      orthoCamRef.current,
+      cameraType
+    );
+  }, [file, cameraType]);
+
+  return (
+    <div className="h-[500px] w-[500px] relative border">
+      <button
+        onClick={toggleCamera}
+        className="absolute z-10 m-2 p-1 bg-white rounded text-black"
+      >
+        Cambiar cámara ({cameraType})
+      </button>
+      <Canvas
+        onCreated={async (state) => {
+          const canvas = state.gl.domElement;
+          if (!navigator.gpu) return;
+
+          const webgpuRenderer = await initWebGPURenderer(canvas);
+          if (!webgpuRenderer) return;
+
+          state.set({ gl: webgpuRenderer as any });
+
+          const loop = () => {
+            state.scene.updateMatrixWorld();
+            state.camera.updateMatrixWorld();
+            webgpuRenderer.renderAsync(state.scene, state.camera);
+            requestAnimationFrame(loop);
+          };
+
+          loop();
+        }}
+      >
+        <PerspectiveCamera
+          ref={perspectiveCamRef}
+          position={[0, 0, zoomLevel]}
+        />
+        <OrthographicCamera ref={orthoCamRef} position={[0, 0, zoomLevel]} />
+
+        <CameraSwitcher
+          cameraType={cameraType}
+          perspectiveRef={perspectiveCamRef}
+          orthoRef={orthoCamRef}
+        />
+
+        <ambientLight intensity={Math.PI / 2} />
+        <Importer />
+        <OrbitControls
+          ref={controlsRef}
+          enableDamping={false}
+          makeDefault
+          onChange={() => {
+            recordRotationChange(controlsRef, dispatch);
+          }}
+        />
+        <OrbitLogger controlsRef={controlsRef} />
+      </Canvas>
+    </div>
+  );
+}
+
+export default Viewport;
--- packages/render/src/views/viewer3d.tsx
+++ packages/render/src/views/viewer3d.tsx
@@ -1,4 +1,4 @@
-import Scene from "../components/scene.tsx";
+import Viewport from "../components/viewport.tsx";
 import RotationDisplay from "../components/rotationDisplay.tsx";
 import MetadataViewer from "../components/metadataViewer.tsx";
 
@@ -7,7 +7,7 @@ function Viewer3d() {
     <>
       <h1 className="text-2xl">3D Viewer</h1>
       <div className="flex flex-row gap-x-2 bg-neutral-800">
-        <Scene />
+        <Viewport />
         <MetadataViewer className="bg-black p-2 rounded-xl h-[165px] text-white" />
         <RotationDisplay className="bg-black p-2 rounded-xl h-[165px] text-white" />
       </div>
