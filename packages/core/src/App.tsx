29c77b4f23113ce1461feadb36f10a858aed9db3
Alejandro <alejandrorodriguezalcala1@gmail.com>
2025-06-01 16:05:55 +0100

    refactor Styles

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,7 +1,9 @@
+import '@astrava/design-system/css/base.css'
 import '@astrava/design-system/css/global.css'
+
 import React, { useState, useEffect, useMemo } from "react";
 import rawTree from '../../file-system/examples/structure.json'
-import {hydrateTree} from "../../file-system/src/utils/toHavenFile.ts";
+import { hydrateTree } from "../../file-system/src/utils/toHavenFile.ts";
 import Renderer from '../../render/src/index.tsx'
 import { FileLoader } from "../../file-system/src/components/fileLoader.tsx";
 import { TreeViewer } from "../../file-system/src/components/treeViewer.tsx";
@@ -9,8 +11,7 @@ import { SearchBar } from "../../file-system/src/components/fileSearcher.tsx";
 import { filterTree } from "../../file-system/src/utils/searcher.ts";
 import { FileSorter } from "../../file-system/src/components/fileSorter.tsx";
 import { sortTreeByNameAsc } from "../../file-system/src/utils/sorter.ts";
-
-import {HavenFile} from './common/havenFile.ts'
+import { HavenFile } from './common/havenFile.ts'
 
 const App: React.FC = () => {
   const hydratedTree = rawTree.map(hydrateTree);
@@ -28,22 +29,37 @@ const App: React.FC = () => {
     }
   }, [searchInput]);
 
-
   const sorterTree = useMemo(() => {
     return sorted ? sortTreeByNameAsc(filteredTree) : filteredTree;
   }, [sorted, filteredTree]);
 
   return (
-    <div className="non-select space-y-4 bg-neutral-800 h-screen text-white p-4">
-      <FileLoader rootDir={rawTree} />
+    <div className="non-select bg-neutral-800 h-screen text-white p-4 space-y-4">
+      {/* Barra de búsqueda general arriba */}
       <SearchBar value={searchInput} onChange={setSearchInput} />
-      <FileSorter sorted={sorted} onToggle={() => setSorted(s => !s)} />
-      <TreeViewer
-        tree={sorterTree}
-        selectedFile={selectedFile}
-        setSelectedFile={setSelectedFile}
-      />
-      {selectedFile && <Renderer file={selectedFile} />}
+
+      <div className="flex h-[calc(100%-100px)] gap-4">
+        {/* Columna izquierda */}
+        <div className="w-1/3 bg-neutral-700 rounded-xl p-4 flex flex-col space-y-4 overflow-hidden">
+          <FileSorter sorted={sorted} onToggle={() => setSorted(s => !s)} />
+          <div className="overflow-y-auto flex-1 pr-2">
+            <TreeViewer
+              tree={sorterTree}
+              selectedFile={selectedFile}
+              setSelectedFile={setSelectedFile}
+            />
+          </div>
+        </div>
+
+        {/* Columna derecha */}
+        <div className="flex-1 bg-neutral-900 rounded-xl p-4 overflow-auto">
+          {selectedFile ? (
+            <Renderer file={selectedFile} />
+          ) : (
+            <div className="text-neutral-500 italic">Selecciona un archivo para ver su contenido</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
