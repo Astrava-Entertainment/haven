d6b4c22a696982bb8c8f63b563c44746bc4d4659
Ellie <24198118+ellie-me@users.noreply.github.com>
2025-07-09 11:07:31 +0000

    Code Suggestion for #11 https://github.com/Astrava-Entertainment/haven/pull/11

--- packages/bramble-parser/index.ts
+++ packages/bramble-parser/index.ts
@@ -1,19 +1,20 @@
+import { Bramble } from "./src/index";
 export { Bramble } from "./src/index"
 
-import { Bramble } from "./src/index";
 
-const bramble = new Bramble("./src/fixtures/example.havenfs");
+//* CLI Entrypoint guard
+if (import.meta.url === `file://${process.argv[1]}`) {
+  const file = process.argv[2] ?? "./src/fixtures/example.havenfs";
+  const bramble = new Bramble(file);
 
-bramble.run();
+  bramble.run();
+  bramble.debugFS();
 
-// bramble.debugChunks();
-bramble.debugFS();
-
-const errors = bramble.getErrors();
-
-if (errors.length > 0) {
-  console.log("Errors found:", errors.length);
-  bramble.logErrors();
-} else {
-  console.log("No errors found");
+  const errors = bramble.getErrors();
+  if (errors.length > 0) {
+    console.log("Errors found:", errors.length);
+    bramble.logErrors();
+  } else {
+    console.log("No errors found");
+  }
 }
--- packages/bramble-parser/src/lexer/index.ts
+++ packages/bramble-parser/src/lexer/index.ts
@@ -1,11 +1,1 @@
 export { BrambleLexer } from "./brambleLexer"
-
-import { BrambleLexer } from "./brambleLexer";
-
-// Example usage:
-// try {
-//   const lexer = new BrambleLexer('./src/fixtures/example.havenfs');
-//   lexer.run();
-//   lexer.getChunkMap();
-//   lexer.debugChunks();
-// } catch (_) { }
