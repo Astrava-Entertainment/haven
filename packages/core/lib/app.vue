e51bdb6b0fc34582434e18175fc51bfe7f47b388
Alejandro <alejandrorodriguezalcala1@gmail.com>
2025-07-14 11:42:36 +0100

    Setup Sass

--- packages/core/lib/app.vue
+++ packages/core/lib/app.vue
@@ -7,9 +7,15 @@ store.initApp()
 </script>
 
 <template>
-  <main class="flex-1 flex flex-col gap-y-4 items-center justify-center min-h-screen bg-primary">
-    <h1 class='text-4xl'>Welcome to <span class='text-visited font-bold'>Haven</span></h1>
-    <img src="/haven_favico.svg" class="size-24" alt="Vue logo"/>
+  <main class="">
     <GreetComponent/>
   </main>
 </template>
+
+<style lang='scss'>
+$primary: red;
+
+.haven {
+  background-color: $primary;
+}
+</style>
