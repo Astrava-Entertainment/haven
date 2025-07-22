b667ffb13b53f0719b1ff609a810adb18e5a441a
Alejandro <alejandrorodriguezalcala1@gmail.com>
2025-07-22 19:39:25 +0100

    Fix error in bramble.ts

--- packages/bramble-parser/bramble.ts
+++ packages/bramble-parser/bramble.ts
@@ -1,40 +1,44 @@
-import { Glob } from "bun";
-import { errorManager } from "./src/errors/errorManager";
-import { BrambleLexer } from "./src/lexer";
-import { BrambleFSParser } from "./src/parser/parser";
+import {Glob} from "bun";
+import {errorManager} from "./src/errors/errorManager";
+import {BrambleLexer} from "./src/lexer";
+import {BrambleFSParser} from "./src/parser/parser";
 
-try {
-  const glob = new Glob("./*.havenfs");
+async function main() {
+  try {
+    const glob = new Glob("./*.havenfs");
 
-  let found = false;
+    let found = false;
 
-  for (const filePath of glob.scan(".")) {
-    found = true;
-    console.log("Found file:", filePath);
+    for await (const filePath of glob.scan(".")) {
+      found = true;
+      console.log("Found file:", filePath);
 
-    const lexer = new BrambleLexer(filePath);
-    lexer.run();
+      const lexer = new BrambleLexer({document: filePath});
+      lexer.run();
 
-    const chunkMap = lexer.getChunkMap();
-    const parser = new BrambleFSParser(chunkMap);
-    parser.run();
-    parser.debugFS();
+      const chunkMap = lexer.getChunkMap();
+      const parser = new BrambleFSParser(chunkMap);
+      parser.run();
+      parser.debugFS();
 
-    const errors = errorManager.getAll();
-    if (errors.length > 0) {
-      console.log("Errors found:", errors.length);
-      errorManager.log();
-    } else {
-      console.log("No errors found in", filePath);
+      const errors = errorManager.getAll();
+      if (errors.length > 0) {
+        console.log("Errors found:", errors.length);
+        errorManager.log();
+      } else {
+        console.log("No errors found in", filePath);
+      }
+
+      errorManager.clear();
     }
 
-    errorManager.clear();
-  }
+    if (!found) {
+      console.log("No .havenfs files found in the current directory.");
+    }
 
-  if (!found) {
-    console.log("No .havenfs files found in the current directory.");
+  } catch (e) {
+    console.error("Unexpected error:", e);
   }
-
-} catch (e) {
-  console.error("Unexpected error:", e);
 }
+
+main();
