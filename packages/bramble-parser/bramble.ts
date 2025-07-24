9a34e7e289b47199578da76c9758f8e4b586cc20
Alejandro <alejandrorodriguezalcala1@gmail.com>
2025-07-24 09:25:07 +0100

    Fix await from bramble.ts

--- packages/bramble-parser/bramble.ts
+++ packages/bramble-parser/bramble.ts
@@ -1,41 +1,42 @@
-import {Glob} from "bun";
-import {errorManager} from "./src/errors/errorManager";
-import {BrambleLexer} from "./src/lexer";
-import {BrambleFSParser} from "./src/parser/parser";
+import { Glob } from "bun";
+import { errorManager } from "./src/errors/errorManager";
+import { BrambleLexer } from "./src/lexer";
+import { BrambleFSParser } from "./src/parser/parser";
 
 async function main() {
   try {
-    const glob = new Glob("./*.havenfs");
+    const glob = new Glob("*.havenfs");
 
-    let found = false;
+    const paths = await Array.fromAsync(glob.scan("."))
 
-    for await (const filePath of glob.scan(".")) {
-      found = true;
-      console.log("Found file:", filePath);
-
-      const lexer = new BrambleLexer({document: filePath});
-      lexer.run();
-
-      const chunkMap = lexer.getChunkMap();
-      const parser = new BrambleFSParser(chunkMap);
-      parser.run();
-      parser.debugFS();
-
-      const errors = errorManager.getAll();
-      if (errors.length > 0) {
-        console.log("Errors found:", errors.length);
-        errorManager.log();
-      } else {
-        console.log("No errors found in", filePath);
-      }
-
-      errorManager.clear();
-    }
-
-    if (!found) {
+    if (paths.length === 0) {
       console.log("No .havenfs files found in the current directory.");
+      return;
     }
 
+    await Promise.all(
+      paths.map(async (filePath) => {
+        console.log("Found file:", filePath);
+
+        const lexer = new BrambleLexer({document: filePath});
+        lexer.run();
+
+        const chunkMap = lexer.getChunkMap();
+        const parser = new BrambleFSParser(chunkMap);
+        parser.run();
+        parser.debugFS();
+
+        const errors = errorManager.getAll();
+        if (errors.length > 0) {
+          console.log("Errors found:", errors.length);
+          errorManager.log();
+        } else {
+          console.log("No errors found in", filePath);
+        }
+
+        errorManager.clear();
+      })
+    );
   } catch (e) {
     console.error("Unexpected error:", e);
   }
